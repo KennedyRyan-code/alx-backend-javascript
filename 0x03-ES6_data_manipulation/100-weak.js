4b1c582492a750c77580d6607f0bcb9b6141716d
KennedyRyan-code <kenryan49@gmail.com>
2024-01-09 12:53:22 +0300

    Weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,16 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    let count = weakMap.get(endpoint);
+    count++;
+    weakMap.set(endpoint, count);
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+  }
+}
+
+export { queryAPI, weakMap };
