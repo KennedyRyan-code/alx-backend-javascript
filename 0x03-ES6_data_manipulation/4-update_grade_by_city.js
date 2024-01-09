dcc505e01370d839bd9c50b5235f277030fa92d0
KennedyRyan-code <kenryan49@gmail.com>
2024-01-09 11:38:35 +0300

    using filter and map combined.

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,21 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const foundGrade = newGrades.find((grade) => grade.studentId === student.id);
+      if (foundGrade) {
+        return {
+          id: student.id,
+          firstName: student.firstName,
+          location: student.location,
+          grade: foundGrade.grade,
+        };
+      }
+      return {
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: 'N/A',
+      };
+    });
+}
