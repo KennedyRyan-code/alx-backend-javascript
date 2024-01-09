98eef75b4dc5d811f7bfcb396f34b9acebfa3a87
KennedyRyan-code <kenryan49@gmail.com>
2024-01-09 15:57:28 +0300

    Return a Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,8 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       // Simulating a successful API response
-      resolve({ data: "Sample API response data" });
+      resolve({ data: 'Sample API response data' });
     }, 1000);
   });
 }
